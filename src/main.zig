ca9a31b95819ac091ae044393b371b098131f248
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-04-04 17:11:00 -0400

    feat: fix clap args

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,68 @@
 const std = @import("std");
+const clap = @import("clap");
+
+const default_max_ticks: u32 = 40_000_000;
 
 const Simulator = struct {
-    max_ticks: u32 = 40_000_000,
+    max_ticks: u32,
     prng: std.Random.DefaultPrng,
 
-    pub fn init(seed: u64) Simulator {
+    pub fn init(seed: u64, max_ticks: u32) Simulator {
         // TigerBeetle uses their own implementation for PRNG
         // For time purposes, we use the stdlib
         // Ref: https://github.com/tigerbeetle/tigerbeetle/blob/78ed407ba07ae674e8feda52dadddba234f4b7f7/src/stdx/prng.zig#L9
-        const prng = std.Random.DefaultPrng.init(seed);
         return Simulator{
-            .prng = prng,
+            .prng = std.Random.DefaultPrng.init(seed),
+            .max_ticks = max_ticks,
         };
     }
 
-    pub fn random_u64(self: Simulator) u64 {
-        // NOTE: don't know why you have to copy a const into a var.
-        // FIX this
-        var prng = self.prng;
-        const rand = prng.random();
-        return rand.int(u64);
+    pub fn random_u64(self: *Simulator) u64 {
+        return self.prng.random().int(u64);
     }
 };
 
 pub fn main() !void {
-    // TODO provide random seed or seed from argument
-    const seed_random = std.crypto.random.int(u64);
 
-    const sim = Simulator.init(seed_random);
-    // It also works when seed is provided by user!
-    // TODO allow CLI args to accept user provided seed
-    // const sim = Simulator.init(0);
+    // To parse CLI args
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    // These are all the CLI args for now
+    // TODO add config file
+    // TODO take params outside of config file
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help            Display this help and exit.
+        \\-s, --seed <u64>      An option parameter, takes a seed
+        \\-t, --ticks <u32>     An option parameter, takes max ticks
+    );
+
+    // Clap parser is here
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .allocator = gpa.allocator(),
+    }) catch {
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    };
+    defer res.deinit();
+
+    // Display help if "-h, --help is called"
+    if (res.args.help != 0) {
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    }
+
+    // Set seed if seed not in args
+    const seed_random = std.crypto.random.int(u64);
+    const seed = if (res.args.seed) |s| s else seed_random;
+
+    // Set max_ticks is ticks not in args
+    const max_ticks = if (res.args.ticks) |t| t else default_max_ticks;
+
+    // Initialize simulator
+    var sim = Simulator.init(seed, max_ticks);
+
+    std.debug.print("seed: {}\n", .{seed});
+    std.debug.print("max_ticks: {}\n", .{max_ticks});
 
     const rand_int = sim.random_u64();
-    std.debug.print("{}\n", .{rand_int});
+    std.debug.print("rand: {}\n", .{rand_int});
 }
