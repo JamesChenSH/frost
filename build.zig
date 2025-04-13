18fc57f58276d037f288076cc2e43959c0794b46
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-04-12 22:56:52 -0400

    patch: remove link to fdb, move to branch

--- build.zig
+++ build.zig
@@ -90,7 +90,4 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
-
-    exe.linkSystemLibrary("fdb.c");
-    exe.addIncludePath("/usr/include/");
 }
